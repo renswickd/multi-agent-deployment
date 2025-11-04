eccf57f3d69ea8694125b9f763ad819ff6a6862c
renswickd <renswick.delver@gmail.com>
2025-11-04 23:26:00 +1300

    agent functionality added

--- app/core/agent.py
+++ app/core/agent.py
@@ -1,5 +1,24 @@
 from langgraph.prebuilt import create_react_agent
 from config.llm import get_llm
+from config.settings import settings
 from core.prompt import SYSTEM_PROMPT
 from core.tools import get_tavily_search
+from langchain_core.messages.ai import AIMessage
 
+def get_agent(llm_id=settings.ALLOWED_MODEL_NAMES[0]):
+    return create_react_agent(
+        llm=get_llm(llm_id),
+        tools=[get_tavily_search(max_results=2)],
+        system_prompt=SYSTEM_PROMPT,
+    )
+
+def get_agent_response(query):
+    agent = get_agent()
+    state = {"messages": query}
+
+    response = agent.invoke(query)
+    messages = response.get("messages", [])
+    if messages:
+        ai_message = [msg.content for msg in messages if isinstance(msg, AIMessage)][-1]
+        return ai_message
+    return ""
--- app/core/tools.py
+++ app/core/tools.py
@@ -1,6 +1,6 @@
 from langchain_tavily import TavilySearch
 from config.settings import settings
 
-def get_tavily_search():
+def get_tavily_search(max_results=settings.MAX_RESULTS):
     return TavilySearch(api_key=settings.TAVILY_API_KEY, 
-                        max_results=settings.MAX_RESULTS)
+                        max_results=max_results)
