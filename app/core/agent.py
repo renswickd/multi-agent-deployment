2e5323c487f1a696a6fe79a412068ca43b146446
renswickd <renswick.delver@gmail.com>
2025-11-04 23:30:46 +1300

    web search tool parameter enabled

--- app/core/agent.py
+++ app/core/agent.py
@@ -5,10 +5,13 @@ from core.prompt import SYSTEM_PROMPT
 from core.tools import get_tavily_search
 from langchain_core.messages.ai import AIMessage
 
-def get_agent(llm_id=settings.ALLOWED_MODEL_NAMES[0]):
+def get_agent(llm_id=settings.ALLOWED_MODEL_NAMES[0], web_search=False):
+
+    tools = [get_tavily_search()] if web_search else []
+
     return create_react_agent(
         llm=get_llm(llm_id),
-        tools=[get_tavily_search(max_results=2)],
+        tools=tools,
         system_prompt=SYSTEM_PROMPT,
     )
 
@@ -16,7 +19,7 @@ def get_agent_response(query):
     agent = get_agent()
     state = {"messages": query}
 
-    response = agent.invoke(query)
+    response = agent.invoke(state)
     messages = response.get("messages", [])
     if messages:
         ai_message = [msg.content for msg in messages if isinstance(msg, AIMessage)][-1]
